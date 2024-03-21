20aeefffb37f6368ba06b501e6d231c1fc4250ff
Timothy Chai <timothy.chai@fdmgroup.com>
2024-03-21 23:25:52 +0800

    Implement CartItemDAO methods
    Implement persist, remove, and findById methods

---
+++ src/main/java/com/fdmgroup/java/timothy_chai_project_ecommerce/repository/CartItemDAO.java
@@ -0,0 +1,36 @@
+package com.fdmgroup.java.timothy_chai_project_ecommerce.repository;
+
+import com.fdmgroup.java.timothy_chai_project_ecommerce.app.CartItem;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
+public class CartItemDAO implements DAO{
+
+	private EntityManagerFactory emf;
+	
+	public CartItemDAO(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+	
+	public void persist(CartItem cartItem) {
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(cartItem);
+        entityManager.getTransaction().commit();
+    }
+	
+	public void remove(CartItem cartItem) {
+		EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.remove(cartItem);
+        entityManager.getTransaction().commit();
+	}
+	
+	public CartItem findById(int cartId) {
+		EntityManager entityManager = emf.createEntityManager();
+        CartItem cartItem = entityManager.find(CartItem.class, cartId);
+        return cartItem;
+        
+	}
+}
