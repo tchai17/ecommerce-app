0e2d1041259f4acdd015c399536ecfa310152e59
Timothy Chai <timothy.chai@fdmgroup.com>
2024-03-21 11:25:54 +0800

    Initialize repository

---
+++ src/main/java/com/fdmgroup/java/timothy_chai_project_ecommerce/App.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.java.timothy_chai_project_ecommerce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
