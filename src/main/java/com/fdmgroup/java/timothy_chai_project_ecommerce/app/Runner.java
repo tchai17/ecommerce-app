6c7aaea0567c7bc7e8e6f8376d71a8648806e425
Timothy Chai <timothy.chai@fdmgroup.com>
2024-03-22 00:31:51 +0800

    Create Runner class to test connection with SQL database

---
+++ src/main/java/com/fdmgroup/java/timothy_chai_project_ecommerce/app/Runner.java
@@ -0,0 +1,50 @@
+package com.fdmgroup.java.timothy_chai_project_ecommerce.app;
+
+import com.fdmgroup.java.timothy_chai_project_ecommerce.repository.CartDAO;
+import com.fdmgroup.java.timothy_chai_project_ecommerce.repository.CartItemDAO;
+import com.fdmgroup.java.timothy_chai_project_ecommerce.repository.CustomerDAO;
+import com.fdmgroup.java.timothy_chai_project_ecommerce.repository.ProductDAO;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		// Create DAO instances
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("MySQL");
+		CustomerDAO customerDAO = new CustomerDAO(emf);
+		CartDAO cartDAO = new CartDAO(emf);
+		CartItemDAO cartItemDAO = new CartItemDAO(emf);
+		ProductDAO productDAO = new ProductDAO(emf);
+		
+		// Create customer instances
+		// (String username, String password, String email, String address, String fullName, String cardNumber)
+		Customer customer1 = new Customer("customer1", "password1", "email1", "address1", "fullName1", "cardNumber1");
+		Customer customer2 = new Customer("customer2", "password2", "email2", "address2", "fullName2", "cardNumber2");
+		
+		// Create product instances
+		Product product1 = new Product("product1", 10, "product1.html", 5.00);
+		Product product2 = new Product("product2", 10, "product2.html", 7.00);
+		
+		// Persist instances
+		customerDAO.persist(customer1);
+		customerDAO.persist(customer2);
+		productDAO.persist(product1);
+		productDAO.persist(product2);
+		
+
+		
+		// Add products to cart
+		CartItem cartItem1 = new CartItem(product1, 5);
+		customer1.getCart().addToCart(cartItem1);
+		customerDAO.persist(customer1);
+
+		
+//		cartDAO.addProductToCart(customer1.getCart(), cartItem1);
+		
+
+	}
+
+}
