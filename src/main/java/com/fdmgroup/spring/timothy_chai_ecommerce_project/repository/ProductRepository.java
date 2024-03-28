9ea88d03dd40b9c6e103c2fe57d772d592d3ca8a
Timothy Chai <timothy.chai@fdmgroup.com>
2024-03-28 12:10:02 +0800

    Minor adjustments to pom.xml and ProductRepository - should not affect application

--- src/main/java/com/fdmgroup/spring/timothy_chai_ecommerce_project/repository/ProductRepository.java
+++ src/main/java/com/fdmgroup/spring/timothy_chai_ecommerce_project/repository/ProductRepository.java
@@ -5,13 +5,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.fdmgroup.spring.timothy_chai_ecommerce_project.model.Customer;
 import com.fdmgroup.spring.timothy_chai_ecommerce_project.model.Product;
 
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 	
-	public List<Product> findByProductName(String productName);	
+	public List<Product> findByProductName(String productName);
+
 
 }
