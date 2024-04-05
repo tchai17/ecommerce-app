61e52ccd5185e0233c6a95e3a9ae67d74134488e
Timothy Chai <timothy.chai@fdmgroup.com>
2024-04-05 08:59:29 +0800

    Clean up code

--- src/main/java/com/fdmgroup/spring/timothy_chai_ecommerce_project/Application.java
+++ src/main/java/com/fdmgroup/spring/timothy_chai_ecommerce_project/Application.java
@@ -14,14 +14,7 @@ public class Application {
 
 	public static void main(String[] args) {
 
-		try {
-			SpringApplication.run(Application.class, args);
-		} catch (Exception e) {
-//			For debugging purposes, use commented-code
-//			e.printStackTrace();
-//			System.out.println(e.getMessage());
-
-		}
+		SpringApplication.run(Application.class, args);
 
 	}
 
